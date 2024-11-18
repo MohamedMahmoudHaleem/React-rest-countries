635a6b0802d49ea7d09f298881e46f63a4a227d2
Mohamed Mahmoud <mohamed.haleem70@hotmail.com>
2024-11-18 18:04:51 +0200

    add icons

--- src/Components/SearchFilterBar.jsx
+++ src/Components/SearchFilterBar.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
+import { Button } from "@/components/ui/button"
 function SearchFilterBar() {
-    return (
-        <div>
-            aaaa
-        </div>
-    )
+    const [country,setCountry] =useState('');
+    function handleCountry(e){
+        setCountry(e.target.value)
+    } 
+    console.log(country)
+  return (
+    <div>
+       <Button>Click me</Button>
+      <input type="text" placeholder="Search fo a country..." 
+      onChange={handleCountry}/>
+    </div>
+  );
 }
 
-export default SearchFilterBar
+export default SearchFilterBar;
